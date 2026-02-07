d2b976e51768577124e1074417c4d082e88ff17c
Alexander Gress <alexander.gress@conduit.hpc.uni-saarland.de>
2026-02-07 21:29:46 +0100

    Adaptions to the HPC

--- structguy/sampleSpace.py
+++ structguy/sampleSpace.py
@@ -1557,7 +1557,7 @@ def put_data_to_tmp_storage(dump_precursor: str, cv_slice: CrossValidationSlice,
 
     gmem = get_gpu_memory()[0]
     sub_share = config.multi_gpu / (config.threads_per_gpu * config.crossValidation_fold * (config.crossValidation_fold-1))
-    num_of_batches = max([1, int(1 * (len(feat_matrix)/ (20 * gmem*sub_share)))])
+    num_of_batches = max([1, int(1 * (len(feat_matrix)/ (50 * gmem*sub_share)))])
 
     batch_size = len(feat_matrix) // num_of_batches
     if len(feat_matrix) % num_of_batches != 0:
